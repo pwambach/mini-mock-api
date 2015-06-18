d7c6326e84d90f643a19cba6f1123a719492b83f
pwambach <philippwambach@googlemail.com>
2015-06-18 22:13:51 +0200

    initial commit

---
+++ example/example.js
@@ -0,0 +1,21 @@
+var API = require('../lib/mini-mock-api');
+
+var server = new API({
+  BASE_PATH: '/api/v2',
+  PORT: 9000,
+  MOCK_FILE_PATH: 'mock-files',
+  ID_ATTRIBUTE: '_id'
+});
+
+//wrap data in 'results' and add total amount
+server.decorate = function(data){
+  if(data.length){
+    return {
+      results: data,
+      total: data.length
+    };
+  }
+  return data;
+};
+
+server.start();
